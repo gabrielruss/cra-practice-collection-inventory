99c79ee3784911c79d852f2b5c33196b68bfd757
Gabriel <gabrielruss88@gmail.com>
2018-02-17 14:57:47 -0500

    about to add a ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import HomePage from './home/HomePage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <HomePage />
       </div>
     );
   }
